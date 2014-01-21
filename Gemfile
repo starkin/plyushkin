16625b64601a589daff0201f7268556f3606407c
Craig Israel and Jeremy Hinkle <dev+craig+jeremy@onlifehealth.com>
2014-01-21 12:26:14 -0600

    adds rspec

--- Gemfile
+++ Gemfile
@@ -2,3 +2,9 @@ source 'https://rubygems.org'
 
 # Specify your gem's dependencies in plyushkin.gemspec
 gemspec
+
+group :test do
+  gem 'rspec'
+  gem 'timecop'
+  gem 'pry-nav'
+end
--- lib/plyushkin.rb
+++ lib/plyushkin.rb
@@ -1,13 +1,14 @@
 require "plyushkin/version"
+require "active_record"
 
 module Plyushkin; end
 path = File.dirname(File.expand_path(__FILE__)) 
-require path + "/plyushkin/lib/base_value"
-require path + "/plyushkin/lib/simple_value"
-require path + "/plyushkin/lib/nil_value"
-require path + "/plyushkin/lib/property"
-require path + "/plyushkin/lib/model"
-require path + "/plyushkin/lib/service"
-require path + "/plyushkin/lib/persistence"
-require path + "/plyushkin/lib/validators/presence"
-require path + "/plyushkin/lib/core_ext/active_record_base"
+require path + "/plyushkin/base_value"
+require path + "/plyushkin/simple_value"
+require path + "/plyushkin/nil_value"
+require path + "/plyushkin/property"
+require path + "/plyushkin/model"
+require path + "/plyushkin/service"
+require path + "/plyushkin/persistence"
+require path + "/plyushkin/validators/presence"
+require path + "/plyushkin/core_ext/active_record_base"
